716529f58c6a989fc719fc410814892ca120e7a0
sarvan.kattamuru <sarvan.kattamuru@thoughtworks.com>
2023-04-15 22:33:45 +0530

    Adds event publisher changes | Sarvan

--- src/main/java/com/example/spark/kafka/extractor/KafkaExtractor.java
+++ src/main/java/com/example/spark/kafka/extractor/KafkaExtractor.java
@@ -17,7 +17,7 @@ public class KafkaExtractor {
         Dataset<Row> dataset = spark.read()
                 .format("kafka")
                 .option("kafka.bootstrap.servers", "localhost:9093")
-                .option("subscribe", "topic1")
+                .option("subscribe", "test_topic")
                 .load()
                 .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)");
         return dataset;
